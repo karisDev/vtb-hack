3f389b60614ef0bf04b9deb632fbd147796b7a8c
karisDev <karjaakirill@gmail.com>
2023-10-13 00:18:44 +0300

    add libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <h1 className='text-red-400'>kru</h1>
     </>
   )
 }
