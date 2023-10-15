8fab3155448158771aaf6483bf8bb845e8c4fc11
algakz06 <77662392+algakz06@users.noreply.github.com>
2023-10-15 07:33:50 +0300

    added endpoint for assistent

--- backend/api/app/endpoints/api.py
+++ backend/api/app/endpoints/api.py
@@ -4,6 +4,7 @@ from app.endpoints.get import router as get_router
 from app.endpoints.select import router as select_router
 from app.endpoints.additional_info import router as additional_info_router
 from app.endpoints.build_route import router as build_route_router
+from app.endpoints.assistent import router as assistent_router
 
 router = APIRouter(
     prefix="/api",
@@ -14,3 +15,4 @@ router.include_router(get_router)
 router.include_router(select_router)
 router.include_router(additional_info_router)
 router.include_router(build_route_router)
+router.include_router(assistent_router)
---
+++ backend/api/app/endpoints/assistent.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, Depends
+from fastapi.responses import HTTPException
+
+
+from app.config import log
+from app.schemas import response_schemas, request_schemas
+from app.utils.calculate_route import calculate_route, add_routes
+from app.ml.departments_analysis import main_departments, predict_1_department
+from app.ml.bancomat_analysis import main_bancomats, predict_1_bancomat
+from app.ml.predict_intent import Adapter
+
+router = APIRouter()
+
+
+@router.get("/assistent", response_model=response_schemas.SelectedOne)
+async def assistent_work(text_prompt: str, latitude: float, longitude: float):
+    adapter = Adapter()
+    intent = adapter.get_response(text_prompt)
+    if intent in ["take_money", "give_money"]:
+        top_atms = main_bancomats(
+            path_to_df="app/data/atms.csv", cur_location=[latitude, longitude]
+        )
+
+        top_atms = add_routes(top_atms, latitude, longitude)
+
+        selected_atm = predict_1_bancomat(top_atms)
+
+        return response_schemas.SelectedOne(id=str(selected_atm))
+    elif intent == "trash":
+        return HTTPException(status_code=422, detail="Not found")
+    else:
+        top_departments = main_departments(
+            path_to_df="app/data/departments.csv",
+            cur_location=[latitude, longitude],
+            operation=intent,
+            is_vip=False,
+            is_person=True,
+            is_juridical=False,
+            is_disabled=False,
+        )
+
+        top_departments = add_routes(
+            top_departments,
+            latitude,
+            longitude,
+        )
+
+        selected_department = predict_1_department(top_departments)
+
+        return response_schemas.SelectedOne(id=str(selected_department))
+
